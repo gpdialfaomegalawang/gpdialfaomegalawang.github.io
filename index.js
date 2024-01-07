552af93b03ed7b6d9127d8d56cbf302ff93fd1b6
NIel <natanoke07@gmail.com>
2024-01-07 20:18:11 +0700

    button fixed

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const a = document.getElementById('nav')
+const button = document.getElementById('burger')
 
 function show() {
-   if (a.style.display === 'flex') {
+   if (a.style.display === 'flex' && button.style.color === 'var(--satin-sheen-gold)') {
     a.style.display = 'none'
+    button.style.color = 'var(--default)'
    }
    else{
     a.style.display = 'flex'
+    button.style.color = 'var(--satin-sheen-gold)'
+    button.style.transition = 'ease-in-out 0.5s'
    }
     
 }
