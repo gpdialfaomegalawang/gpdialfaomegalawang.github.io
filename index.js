02037e7c15be0c3f4741e8fac787608067132daa
NIel <natanoke07@gmail.com>
2024-01-07 18:51:52 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const a = document.getElementById('nav')
+
+function show() {
+   if (a.style.display === 'flex') {
+    a.style.display = 'none'
+   }
+   else{
+    a.style.display = 'flex'
+   }
+    
+}
